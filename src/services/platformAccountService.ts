08f4d33517b4a875d5de8b919824c6ecd4dc60c1
salahmostava-sys <salahmostava@gmail.com>
2026-03-24 10:37:40 +0300

    Create platformAccountService.ts for account management functions

---
+++ src/services/platformAccountService.ts
@@ -0,0 +1,43 @@
+// platformAccountService.ts
+
+/**
+ * This service manages platform accounts.
+ */
+
+export class PlatformAccountService {
+
+    /**
+     * Get all platform accounts.
+     */
+    static getAllAccounts() {
+        // Implementation here
+        return [];
+    }
+
+    /**
+     * Create a new platform account.
+     * @param account - The account to create.
+     */
+    static createAccount(account: any) {
+        // Implementation here
+        return account;
+    }
+
+    /**
+     * Update an existing platform account.
+     * @param accountId - The ID of the account to update.
+     * @param account - The new account data.
+     */
+    static updateAccount(accountId: string, account: any) {
+        // Implementation here
+        return account;
+    }
+
+    /**
+     * Get accounts with expiring iqama.
+     */
+    static getAccountsWithExpiringIqama() {
+        // Implementation here
+        return [];
+    }
+}
